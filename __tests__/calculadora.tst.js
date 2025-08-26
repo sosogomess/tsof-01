99bc7b79f108d8260b448b929ddd00cf0125010f
Sophia Gomes <sophiagomes.tds2.senai@gmail.com>
2025-08-26 10:48:06 -0300

    feat: create subtrair test

--- __tests__/calculadora.tst.js
+++ __tests__/calculadora.tst.js
@@ -1,4 +1,4 @@
-import { somar } from "../src/calculadora.js";
+import { somar, subtrair } from "../src/calculadora.js";
 
 describe("Calculadora", () => {
   test("Deve somar dois números corretamente", () => {
@@ -14,3 +14,17 @@ describe("Calculadora", () => {
     expect(resultado).toBe(5);
   });
 });
+
+test("Deve subtrair dois números corretamente", () => {
+    //AAA
+    //Arange (preparar)
+    const a = 13;
+    const b = 3;
+
+    //Act (Executar)
+    const resultado = subtrair(a, b);
+
+    //Assert (Verificar)
+    expect(resultado).toBe(10);
+  });
+
--- src/calculadora.js
+++ src/calculadora.js
@@ -2,3 +2,7 @@
 export function somar(a, b) {
   return a + b;
 }
+
+export function subtrair(a, b) {
+    return a - b;
+}
